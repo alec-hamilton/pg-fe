44f9e621d4630f122964a97b2eda1f47724698b2
alech <hamiltonalec@gmail.com>
2022-10-07 11:49:45 +0100

    changed to use internal data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import './styles.css';
-import {useEffect, useState} from "react";
-import Season1 from "./Components/Season1";
+import Season from './components/Season';
+import { season1Data } from './services/EpisodeData';
+import { season2Data } from './services/EpisodeData';
+import { season3Data } from './services/EpisodeData';
 
 function App() {
 
-    const [episodeItems, setEpisodeItems] = useState([]);
-
-    const fetchData = async () => {
-        const response = await fetch('http://localhost:3001/episodes');
-
-        if (!response.ok) {
-            throw new Error('Data could not be fetched.')
-        }
-
-        return await response.json();
-    }
-
-    useEffect(() => {
-            fetchData()
-                .then((episodeData) => {
-                    setEpisodeItems(episodeData);
-                })
-                .catch((e) => {
-                    console.log(e.message);
-            })
-        }, []
-    );
-
     return (
-        <Season1 episodeItems={episodeItems} />
+        <>
+        <Season seasonData={season3Data} />
+        <Season seasonData={season2Data} />
+        <Season seasonData={season1Data} />
+        </>
     );
 }
 
