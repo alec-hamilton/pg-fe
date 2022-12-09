cb9de7d2fe14bf26df05123b10fe967e8161d438
alech <hamiltonalec@gmail.com>
2022-12-09 15:21:24 +0000

    seasonContainer done

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,6 @@
 import Season from "./components/Season/Season";
 import List from "./components/List/List";
+import { SeasonContainer } from "./Home.styles";
 import mixData from "../../data/mixData";
 
 const Home = () => {
@@ -7,11 +8,11 @@ const Home = () => {
     <>
       {mixData.map((season) => {
         return (
-          <div className="main-container" key={season.details.season}>
+          <SeasonContainer key={season.details.season}>
             <Season seasonData={season} />
             <List seasonData={season} />
             <hr />
-          </div>
+          </SeasonContainer>
         );
       })}
     </>
