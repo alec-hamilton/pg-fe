7ab34710537807061df9c66e14116837655dae9d
alech <hamiltonalec@gmail.com>
2022-12-15 14:17:06 +0000

    global styles sorted

--- src/components/Typography/Typography.styles.js
+++ src/components/Typography/Typography.styles.js
@@ -3,7 +3,6 @@ import styled from "styled-components";
 export const SeasonTitle = styled.h2`
   padding: 0;
   margin: 1.2rem 0.4rem 0 0.4rem;
-  color: var(--white);
   font-weight: 500;
 `;
 
@@ -14,7 +13,6 @@ export const ArtistLine = styled.p`
 `;
 
 export const HoverTextArtist = styled.h3`
-  color: var(--white);
   font-weight: 500;
   font-size: 2.5vw;
   padding-top: 5px;
@@ -28,7 +26,6 @@ export const HoverTextArtist = styled.h3`
 `;
 
 export const HoverTextSeasonEpisode = styled.h4`
-  color: var(--white);
   font-weight: 300;
   font-size: 2vw;
   padding: 0;
@@ -41,10 +38,7 @@ export const HoverTextSeasonEpisode = styled.h4`
 
 export const ListEpisodeText = styled.p`
   font-weight: 500;
-  color: var(--white);
   text-decoration: underline;
-  transition: 0.3s;
-  transition-property: color;
   margin: 0 0 0 1rem;
   display: inline;
 `;
@@ -52,11 +46,9 @@ export const ListEpisodeText = styled.p`
 export const PageTitle = styled.h1`
   padding: 0;
   margin: 1.2rem 0;
-  color: var(--white);
   font-weight: 500;
 `;
 
 export const BodyText = styled.p`
-  color: var(--white);
   font-weight: 500;
 `;
