83e93b9326cf85d4feead851553510791beded25
alech <hamiltonalec@gmail.com>
2022-12-19 13:20:45 +0000

    changes reflect working vite project

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import svgrPlugin from 'vite-plugin-svgr';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgrPlugin()],
-  server: {
-    open: true,
-    port: 4001,
-  },
-});
+  plugins: [react()],
+})
